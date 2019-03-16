fe63123c74f00d94ff7a103e8462c4abcaec6d73
icefiresS <me@icefires.cc>
2019-03-16 17:04:37 +0800

    Add a lot of new functions.

---
+++ app.py
@@ -0,0 +1,49 @@
+import flask, os, json
+from flask import request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+from bot import get_time
+
+app = flask.Flask(__name__)
+
+class InfoForm(FlaskForm):
+    name = StringField('姓名', validators=[DataRequired()])
+    idnum = StringField('学号', validators=[DataRequired()])
+    url = StringField('抢讲座链接', validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    back_log = os.path.join('back_log', 'back.json')
+
+    if request.method == 'POST':
+        try:
+            form = request.form
+            data = form.to_dict()
+            data.pop('submit')
+            data['time'] = get_time(data['url'])
+            data['status'] = 'waiting'
+            
+            with open(back_log) as f:
+                origin = json.load(f)
+            origin.append(data)
+            with open(back_log, 'w') as f:
+                json.dump(origin, f)
+        except:
+            print('Parsing data failed.')
+        
+        if data['time'] != None:
+            os.system('python3 bot.py --url %s --name %s --id %s &' % (data['url'], data['name'], data['idnum']))
+
+    with open(back_log) as f:
+        infos = json.load(f)
+
+    form = InfoForm(meta={'csrf': False})
+    return flask.render_template('index.html', form=form, infos=infos)
+
+if __name__ == "__main__":
+    app.run(debug=False)
--- bot.py
+++ bot.py
@@ -4,6 +4,26 @@ import json, time
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.parse import urlparse
+import argparse
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime, timedelta
+
+def get_time(url):
+    try:
+        html = urlopen(url).read()
+        soup = BeautifulSoup(html,"html.parser")
+        titles = soup.select("head  script")
+        if titles[0].text.strip().split('\n')[5].strip().startswith('var SOUL'):
+            info = titles[0].text.strip().split('\n')[5].strip()[11:-1]
+        else:
+            info = titles[0].text.strip().split('\n')[6].strip()[11:-1]
+        content = json.loads(info)
+        date = content['se']['apf']
+    except:
+        date = None
+    
+    return date
 
 def create_post_data(origin_url, name, _id):
     parse = urlparse(origin_url)
@@ -36,20 +56,33 @@ def create_headers(origin_url):
         'Origin': "http://%s" % parse.hostname,
         'Referer': origin_url,
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
-        'Cookie': 'uvi=wU8tHtkSeVmpXsMnr0WqxB0JHiWSAOuzf7ThqRmKMsjS7i01; PHPSESSID=3demtdaaip4jni27r8p7lo3h1k',
         'X-Requested-With': 'XMLHttpRequest'
     }
     return headers
 
+def get_table(name, _id, origin_url):
+    back_log = 'back_log/back.json'
+    with open(back_log) as f:
+        table = json.load(f)
+    for idx, info in enumerate(table):
+        if name == info['name'] and _id == info['idnum'] and origin_url == info['url']:
+            request_idx = idx
+            break
+    return (table, request_idx)
+
 def post_request(origin_url, name, _id):
+    back_log = 'back_log/back.json'
     parse = urlparse(origin_url)
     data = create_post_data(origin_url, name, _id)
     headers = create_headers(origin_url)
     info = '(表单: %s, 姓名: %s, 学号: %s)' % (origin_url, name, _id)
     
     if data == None:
-        return '[%s] 查询失败! (%s)' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), info)
-        return
+        table, request_idx = get_table(name, _id, origin_url)
+        table[request_idx]['status'] = 'fail'
+        with open(back_log, 'w') as f:
+            json.dump(table, f)
+        print('[%s] 查询失败! (%s)' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), info))
 
     url = 'http://%s/handler/web/form_runtime/handleSubmit.php' % parse.hostname
     try:
@@ -57,14 +90,35 @@ def post_request(origin_url, name, _id):
         s.mount('http://', HTTPAdapter(max_retries=5))
         r = s.post(url, data=data, headers=headers, timeout=5)
         if r.status_code == 200 and r.text == '{"r":0}':
-            return '[%s] 发送成功! (%s)' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), info)
+            table, request_idx = get_table(name, _id, origin_url)
+            table[request_idx]['status'] = 'success'
+            print('[%s] 发送成功! (%s)' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), info))
         else:
-            return '[%s] 发送失败. (%s)' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), info)
+            table, request_idx = get_table(name, _id, origin_url)
+            table[request_idx]['status'] = 'fail'
+            print('[%s] 发送失败. (%s)' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), info))
     except requests.ConnectionError as e:
-        return '[%s] 连接失败. (%s)' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), info) + e.args
+        table, request_idx = get_table(name, _id, origin_url)
+        table[request_idx]['status'] = 'fail'
+        print('[%s] 连接失败. (%s)' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), info) + e.args)
+
+    with open(back_log, 'w') as f:
+        json.dump(table, f)
+
 
 if __name__ == '__main__':
-    origin_url = 'http://tk5rrvu0xdnrh3el.mikecrm.com/sVQqA5l'
-    name = '陈海天'
-    _id = '3150104457'
-    print(post_request(origin_url, name, _id))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', type=str)
+    parser.add_argument('--name', type=str)
+    parser.add_argument('--id', type=str)
+
+    opt = parser.parse_args()
+    url = opt.url
+    name = opt.name
+    _id = opt.id
+    
+    t = datetime.strptime(get_time(url), "%Y-%m-%d %H:%M:%S")
+    t = t + timedelta(seconds=1)
+    scheduler = BlockingScheduler()
+    scheduler.add_job(post_request, args=[url, name, _id], run_date=t)
+    scheduler.start()
